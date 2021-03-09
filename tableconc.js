ff336feed64ce111b75b011de8291ad58ef0810f
Herman Leung <leung.hm@gmail.com>
2021-03-09 22:51:58 +0800

    Flesh out search form

--- tableconc.js
+++ tableconc.js
@@ -1,3 +1,40 @@
+const fileInput = document.getElementById("csv");
 
+const readFile = function () {
+    var reader = new FileReader();
+    reader.onload = function () {    
+        data = $j.csv.toArrays(reader.result);
+        
+        // display all column names
+        d3.select("#column-names")
+            .select("ul")
+            .selectAll("li")
+            .data(data[0])
+            .enter().append("li")
+                .text(function(d) { return d; });
+
+        // populate drop-down menu
+        d3.select("#column-selection")
+            .selectAll("option")
+            .data(data[0])
+            .enter().append("option")
+                .attr("value", function(d) { return d; })
+                .text(function(d) { return d; });
+        
+        // columns to show
+        var columnsToShow = d3.select("#columns-to-show").selectAll("input")
+            .data(data[0]).enter();
+        columnsToShow.append("input")
+            .attr("id", function(d) { return "to-show-" + d; })
+            .attr("type", function () { return "checkbox"; });
+        columnsToShow.insert("label")
+            .attr("for", function(d) { return "to-show-" + d; })
+            .text(function(d) { return d; });
+
+    };
+    reader.readAsBinaryString(fileInput.files[0]);
+};
+
+fileInput.addEventListener('change', readFile);
 
 
