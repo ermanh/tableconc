2542bf2f1a86c80285881a3d737b534ca6c008f6
Herman Leung <leung.hm@gmail.com>
2021-06-17 21:12:18 +0800

    Refactoring in progress

--- search.js
+++ search.js
@@ -25,16 +25,9 @@ function prepareColumns(columnNames) {
     const columnsToDisplay = columnNames.filter((d, i) => {
         if (selectedColumns[i]) { return d; }
     });
-    // Add original line number
-    selectedColumns.unshift(true);
-    columnsToDisplay.unshift("");
-    // Add result index column with text-align control
-    selectedColumns.unshift(true);
-    columnsToDisplay.unshift(`
-        <svg id="text-align-control" class="align-left" height="13", width="15">
-            <path d="M0,3 L8,3 M0,6 L13,6 M0,9 L8,9 M0,12 L13,12"/>
-        </svg>
-    `); 
+    // Add original line number & result index column
+    selectedColumns.unshift(true, true);
+    columnsToDisplay.unshift("", ""); 
     return [selectedColumns, columnsToDisplay];
 }
 
@@ -48,29 +41,30 @@ function determineThClasses(i) {
     }
 }
 
-function determineThInnerHTML(d, i, columnsToDisplayLength) {
-    // Add resize and sorter controller divs in header row
-    if (i == 0) {
-        return d;
-    } else if (i == 1) {
-        return `
-            <div class="sort line-number" id="i${i}">&#x25B2;</div>`; 
+function determineThInnerHTML(d, i, totalColumns) {
+    if (i == 0) {  // return text aligner only
+        return `<svg id="text-align-control" class="align-left" 
+                 height="13", width="15">
+                    <path d="M0,3 L8,3 M0,6 L13,6 M0,9 L8,9 M0,12 L13,12"/>
+                </svg>`;
+    } else if (i == 1) {  // return sorter for original-index column
+        return `<div class="sort line-number" id="i${i}">&#x25B2;</div>`; 
     } else if (i == 2) {
-        if (i == columnsToDisplayLength - 1) {
+        if (i == totalColumns - 1) {  // add sorter only
             return `<pre>${d}</pre>
                     <div class="sort" id="i${i}">&equiv;</div>`; 
-        } else {
+        } else {  // add sorter & right-side column resizer
             return `<pre>${d}</pre>
                     <div class="sort" id="i${i}">&equiv;</div>
                     <div class="resize-right"></div>`; 
         }
     } else {
-        if (i !== columnsToDisplayLength - 1) {
+        if (i !== totalColumns - 1) {  // add sorter & column resizers
             return `<div class="resize-left"></div>
                     <pre>${d}</pre>
                     <div class="sort" id="i${i}">&equiv;</div>
                     <div class="resize-right"></div>`; 
-        } else {
+        } else {  // add sorter & left-side column resizer
             return `<div class="resize-left"></div>
                     <pre>${d}</pre>
                     <div class="sort" id="i${i}">&equiv;</div>`;
